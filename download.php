f2e60befb5105642943e647f5f6b5a9756fbef93
Kenijo <kenijo@users.noreply.github.com>
2025-01-23 13:01:49 -0800

    Create the code to initiate the file download

---
+++ download.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_GET['zipFilename'])) {
+    $zipFilename = urldecode($_GET['zipFilename']);
+    header('Content-Type: application/zip');
+    header('Content-Disposition: attachment; filename="npp-theme.zip"');
+    header('Content-Length: ' . filesize($zipFilename));
+    readfile($zipFilename);
+    unlink($zipFilename);
+}
